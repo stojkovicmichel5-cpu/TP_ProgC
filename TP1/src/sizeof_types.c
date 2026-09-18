36f71fadd2124953b5fef5914ded8b623eedea69
stojkovicmichel5-cpu <stojkovic.michel5@gmail.com>
2026-09-18 13:41:19 +0000

    commit

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main()
+{
+    printf("char              : %zu octets\n", sizeof(char));
+    printf("signed char        : %zu octets\n", sizeof(signed char));
+    printf("unsigned char      : %zu octets\n", sizeof(unsigned char));
+
+    printf("short              : %zu octets\n", sizeof(short));
+    printf("signed short       : %zu octets\n", sizeof(signed short));
+    printf("unsigned short     : %zu octets\n", sizeof(unsigned short));
+
+    printf("int                : %zu octets\n", sizeof(int));
+    printf("signed int         : %zu octets\n", sizeof(signed int));
+    printf("unsigned int       : %zu octets\n", sizeof(unsigned int));
+
+    printf("long int           : %zu octets\n", sizeof(long int));
+    printf("signed long int    : %zu octets\n", sizeof(signed long int));
+    printf("unsigned long int  : %zu octets\n", sizeof(unsigned long int));
+
+    printf("long long int      : %zu octets\n", sizeof(long long int));
+    printf("signed long long   : %zu octets\n", sizeof(signed long long int));
+    printf("unsigned long long : %zu octets\n", sizeof(unsigned long long int));
+
+    printf("float              : %zu octets\n", sizeof(float));
+    printf("double             : %zu octets\n", sizeof(double));
+    printf("long double        : %zu octets\n", sizeof(long double));
+
+    return 0;
+}
